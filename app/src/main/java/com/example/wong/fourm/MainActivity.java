6c9cede61db5231092a7feb2cfcd7815046e4e3b
DESKTOP-P9I81RM\wong <a26bc48@gmail.com>
2019-04-28 15:57:00 +0800

    init project

---
+++ app/src/main/java/com/example/wong/fourm/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.wong.fourm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.lv_item);
+        lv.setAdapter(new MyListAdapter());
+    }
+
+    private class MyListAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
+}
