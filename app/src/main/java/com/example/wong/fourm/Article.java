1386525974d4990e8be54bd504dca52c0e2c75c4
DESKTOP-P9I81RM\wong <a26bc48@gmail.com>
2019-04-29 19:29:39 +0800

    partly finish item onclick

---
+++ app/src/main/java/com/example/wong/fourm/Article.java
@@ -0,0 +1,88 @@
+package com.example.wong.fourm;
+
+public class Article {
+    private String id;
+    private String title;
+    private String body;
+    private String author_id;
+    private String author_name;
+    private String create_date;
+    private String url;
+    private String type;
+    private int count = 0;
+
+    public Article() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(String author_id) {
+        this.author_id = author_id;
+    }
+
+    public String getAuthor_name() {
+        return author_name;
+    }
+
+    public void setAuthor_name(String author_name) {
+        this.author_name = author_name;
+    }
+
+    public String getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(String create_date) {
+        this.create_date = create_date;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
--- app/src/main/java/com/example/wong/fourm/MainActivity.java
+++ app/src/main/java/com/example/wong/fourm/MainActivity.java
@@ -1,28 +1,114 @@
 package com.example.wong.fourm;
 
+
+import android.app.Activity;
+import android.content.Intent;
+import android.location.Location;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.ListAdapter;
+
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.StringCallback;
+import com.lzy.okgo.model.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
     private ListView lv;
+    private JSONObject obj;
+    private ArrayList<Article> articleList;
+    private int count = 0;
+
+    protected Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        OkGo.getInstance().init(getApplication());
         setContentView(R.layout.activity_main);
         lv = (ListView) findViewById(R.id.lv_item);
-        lv.setAdapter(new MyListAdapter());
+
+//        GPSUtils gpsUtils = new GPSUtils(this);
+//        String address = gpsUtils.getAddressStr();
+//        Toast.makeText(this, address, Toast.LENGTH_LONG).show();
+
+
+        OkGo.<String>get("http://192.168.1.2:3000/articles").execute(new StringCallback() {
+
+
+            @Override
+            public void onSuccess(Response<String> response) {
+                Log.d("hello", response.body());
+                try {
+                    articleList = new ArrayList<Article>();
+                    obj = new JSONObject(response.body());
+                    count = Integer.parseInt(obj.getString("count"));
+                    JSONArray articles = obj.getJSONArray("articles");
+                    for (int i = 0; i < articles.length(); i++) {
+                        JSONObject article = (JSONObject) articles.get(i);
+                        //取出name
+                        Article article1 = new Article();
+                        article1.setId(article.getString("_id"));
+                        article1.setTitle(article.getString("title"));
+                        article1.setBody(article.getString("body"));
+                        article1.setCreate_date(article.getString("create_date"));
+                        JSONObject author = article.getJSONObject("author");
+                        JSONObject request_data = article.getJSONObject("request");
+                        article1.setAuthor_name(author.getString("name"));
+                        article1.setAuthor_id(author.getString("_id"));
+                        article1.setUrl(request_data.getString("url"));
+                        article1.setType(request_data.getString("type"));
+
+                        articleList.add(article1);
+                    }
+
+                    lv.setAdapter(new MyListAdapter());
+                    lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                            Article article = articleList.get(i);
+                            Toast.makeText(getApplicationContext(), article.getTitle(), Toast.LENGTH_LONG).show();
+
+                        }
+                    });
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onError(Response<String> response) {
+                Log.d("hello", "fuck");
+                super.onError(response);
+            }
+        });
+
+
     }
+
 
     private class MyListAdapter extends BaseAdapter {
         @Override
         public int getCount() {
-            return 0;
+            return count;
         }
 
         @Override
@@ -37,7 +123,20 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            return null;
+            View view;
+            if (convertView == null) {
+                view = View.inflate(getApplicationContext(), R.layout.item_layout, null);
+
+            } else {
+                view = convertView;
+            }
+            TextView tv_title = (TextView) view.findViewById(R.id.tv_title);
+            TextView tv_body = (TextView) view.findViewById(R.id.tv_body);
+            tv_title.setText(articleList.get(position).getTitle());
+            tv_body.setText(articleList.get(position).getBody());
+            return view;
         }
     }
+
+
 }
