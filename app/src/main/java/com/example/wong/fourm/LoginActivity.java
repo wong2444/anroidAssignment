4449123c75ecc307cf4fbdb2096dcd0191f16e41
DESKTOP-P9I81RM\wong <a26bc48@gmail.com>
2019-05-01 01:20:20 +0800

    internationalization finish

--- app/src/main/java/com/example/wong/fourm/LoginActivity.java
+++ app/src/main/java/com/example/wong/fourm/LoginActivity.java
@@ -23,22 +23,26 @@ import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
     EditText et_email;
+    EditText et_name;
     EditText et_password;
     Button btn_login;
+    Button btn_register;
     private JSONObject obj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        et_name = (EditText) findViewById(R.id.et_name);
         et_email = (EditText) findViewById(R.id.et_email);
         et_password = (EditText) findViewById(R.id.et_password);
         btn_login = (Button) findViewById(R.id.btn_login);
+        btn_register = (Button) findViewById(R.id.btn_register);
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(getApplicationContext(), "login load", Toast.LENGTH_LONG).show();
+
                 String email = et_email.getText().toString().trim();
                 String password = et_password.getText().toString().trim();
                 OkGo.<String>post("http://192.168.1.2:3000/user/login")
@@ -66,6 +70,56 @@ public class LoginActivity extends AppCompatActivity {
                                         Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_LONG).show();
                                         et_email.setText("");
                                         et_password.setText("");
+                                    }
+
+
+                                } catch (JSONException e) {
+                                    Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_LONG).show();
+                                    e.printStackTrace();
+                                }
+
+                            }
+
+                            @Override
+                            public void onError(Response<String> response) {
+                                Toast.makeText(getApplicationContext(), "error", Toast.LENGTH_LONG).show();
+                                Log.d("hello", "fuck");
+                                super.onError(response);
+                            }
+                        });
+            }
+        });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = et_email.getText().toString().trim();
+                String password = et_password.getText().toString().trim();
+                String name = et_name.getText().toString();
+                OkGo.<String>post("http://192.168.1.2:3000/user/signup")
+                        .params("email", email)
+                        .params("password", password)
+                        .params("name", name)
+                        .execute(new StringCallback() {
+
+
+                            @Override
+                            public void onSuccess(Response<String> response) {
+                                Log.d("hello", response.body());
+                                try {
+
+                                    obj = new JSONObject(response.body());
+                                    if (obj.isNull("error")) {
+                                        et_email.setText("");
+                                        et_password.setText("");
+                                        et_name.setText("");
+
+                                        Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_LONG).show();
+
+                                    } else {
+
+                                        Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_LONG).show();
+
                                     }
 
 
